c3d21a224cbec2683c0ae5988f6c0ea63236fcdf
firozalam <firozalam.786123@gmail.com>
2025-05-17 10:32:04 +0530

    initial commit

---
+++ helpers/cloudinary.js
@@ -0,0 +1,25 @@
+// filepath: /Users/firozalam/Projects/mern_project/mern-ecommerce-2024/server/helpers/cloudinary.js
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+require("dotenv").config(); // Load environment variables
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
