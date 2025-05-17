2336f0286faa4a89e8e3328d5f6dd79d54f70b96
firozalam <firozalam.786123@gmail.com>
2025-05-17 15:20:06 +0530

    cors enabled for all

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
